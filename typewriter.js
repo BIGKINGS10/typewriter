923cdbd54f1e47ce5c9627e4a1ef4cd325edee1f
BIGKINGS10 <adventbigkings@gmail.com>
2023-04-24 09:02:23 +0100

    Complete typewritter.js file

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+
+  
+let delay = 0;
+for (let char of sentence) {
+  setTimeout(() => process.stdout.write(char), delay);
+  delay += 500;
+}
+  
+setTimeout(() => {
+  console.log('');
+}, delay);
+  
